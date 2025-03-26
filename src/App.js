9376574871f4c87dd439211b1ffdc8c66006dc7a
VianneyLA <vianney.boillot@logiadapt.fr>
2025-03-26 08:40:12 +0100

    Switch to HashRouter for GitHub Pages compatibility

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate, Link } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate, Link } from 'react-router-dom';
 import Header from './components/Header';
 import StepOne from './components/StepOne';
 import StepTwo from './components/StepTwo';
